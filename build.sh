9ca6d0ba7c5635ab5633826cfe0ccbfe90d794ae
Christopher Wong <christopherpk1996@gmail.com>
2017-11-15 16:02:17 +0800

    build script index

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-react-native bundle --dev false --platform android --entry-file index.android.js --bundle-output ./android/app/build/intermediates/assets/debug/index.android.bundle --assets-dest ./android/app/build/intermediates/res/merged/debug
+react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/build/intermediates/assets/debug/index.android.bundle --assets-dest ./android/app/build/intermediates/res/merged/debug
 cd android/
 ./gradlew assembleDebug
 cd app/

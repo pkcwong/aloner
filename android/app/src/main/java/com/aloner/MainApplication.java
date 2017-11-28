a8f5c14c0707cd61a26fe1ae22a0b30864ad354f
yy0234 <yunglp0234@gmail.com>
2017-11-29 00:54:02 +0800

    debug

--- android/app/src/main/java/com/aloner/MainApplication.java
+++ android/app/src/main/java/com/aloner/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.imagepicker.ImagePickerPackage;
+import com.imagepicker.ImagePickerPackage;
 
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
 		protected List<ReactPackage> getPackages() {
 			return Arrays.<ReactPackage>asList(
 					new MainReactPackage(),
+            new ImagePickerPackage(),
                     new ImagePickerPackage(),
 					new RNFirebasePackage(),
 					new RNFirebaseAuthPackage(),
